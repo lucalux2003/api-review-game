70b92e46826a744afb0854baa64ea3ae5e535f6c
lucalux2003 <lucgomez777@gmail.com>
2024-10-10 16:28:57 +0200

    exercice 3.2

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Route, Tags } from "tsoa";
+import { Body, Controller, Get, Route, Tags, Path } from "tsoa";
 import { GameDTO } from "../dto/game.dto";
 import { gameService } from "../services/game.service";
 
@@ -8,5 +8,10 @@ export class GameController extends Controller {
   @Get("/")
   public async getAllGames(): Promise<GameDTO[]> {
     return gameService.getAllGames();
+  }
+
+  @Get("{id}")
+  public async getGameById(@Path() id: number): Promise<GameDTO | null> {
+    return gameService.getGameById(id);
   }
 }
--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,6 +1,8 @@
 import { GameDTO } from "../dto/game.dto";
 import { Console } from "../models/console.model";
 import { Game } from "../models/game.model";
+import { notFound } from "../error/NotFoundError";
+
 
 export class GameService {
   public async getAllGames(): Promise<GameDTO[]> {
@@ -12,6 +14,13 @@ export class GameService {
         },
       ],
     });
+  }
+  public async getGameById(id: number): Promise<GameDTO | null> {
+    const game = await Game.findByPk(id);
+    if(game){
+      return game;
+    }
+    return notFound("Game id " + id);
   }
 }
 
