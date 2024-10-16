4dfe2236b7e9969a1443163020c2bf1f354c66d5
lucalux2003 <lucgomez777@gmail.com>
2024-10-16 14:09:15 +0200

    Question 4.1

--- src/models/review.model.ts
+++ src/models/review.model.ts
@@ -1,19 +1,47 @@
 import { DataTypes, Model } from "sequelize";
 import sequelize from "../config/database"; // Connexion à la base de données
+import { Game } from "./game.model";
 
 export interface ReviewAttributes {
+  id?: number;
+  rating: number;
+  text_review: string;
+  game_id: number;
+  game?: Game;
 }
 
-export class Review
-  extends Model<ReviewAttributes>
-  implements ReviewAttributes
-{
+export class Review extends Model<ReviewAttributes> implements ReviewAttributes{
+  public id!: number;
+  public rating!: number;
+  public text_review!: string;
+  public game_id!: number;
+  public game!: Game;
 }
 
 Review.init(
-  {},
+  {
+    id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  rating: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  text_review: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  game_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+},
   {
     sequelize,
     tableName: "reviews",
   }
 );
+
+Review.belongsTo(Game, { foreignKey: "game_id", as: "game" });
